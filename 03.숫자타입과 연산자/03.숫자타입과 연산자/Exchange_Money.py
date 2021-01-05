6f89a555ee4641f7530234f865064175f9ce49b0
ParkDoHun <55828180+ParkDoHun@users.noreply.github.com>
2021-01-05 15:54:15 +0900

    Add files via upload

---
+++ 03.숫자타입과 연산자/03.숫자타입과 연산자/Exchange_Money.py
@@ -0,0 +1,23 @@
+#### 환율 계산기 ####
+# 환전 금액 입력
+exchange_in = eval(input('환전 금액을 입력하세요. : '))  # eval : 자동 형 변환
+
+# 각국의 환율(2017년 08월 19일 기준)
+print('\n\n======각국의 환율======')
+print('- 미국 :1,141.50원/USD')
+print('- 호주 : 905.15원/AUD')
+print('- 뉴질랜드 : 836.15원/NZD')
+
+print('\n======변환된 환율======')
+
+# 각 변수에 연산값 저장
+usd = round(exchange_in / 1141.50,2)
+aud = round(exchange_in / 905.15, 2)
+nzd = round(exchange_in / 836.15, 2)
+
+# 환울 변환 출력
+print('미국 :', float(usd), 'USD')
+print('호주 :', float(aud), 'AUD')
+print('뉴질랜드', float(nzd), 'NZD')
+
+
