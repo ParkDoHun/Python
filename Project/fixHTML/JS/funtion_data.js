745f25e9a6f4cae98d1f1810597c70a337c79305
ParkDoHun <55828180+ParkDoHun@users.noreply.github.com>
2021-02-17 19:05:48 +0900

    Add files via upload

--- Project/fixHTML/JS/funtion_data.js
+++ Project/fixHTML/JS/funtion_data.js
@@ -99,7 +99,7 @@ function next_Page(tid, count, number) {
 
     //값 저장 후 페이지 이동
     localStorage.setItem("allEntries", JSON.stringify(existingEntries));
-    location.href = "http://127.0.0.1:5500/html"+'/'+number+'~'+".html?"+number;
+    location.href = "http://127.0.0.1:5500/Project/fixHTML/html/"+number+'~'+".html?"+number;
 };
 
 /*이전 페이지로 이동*/
@@ -125,13 +125,14 @@ function previous_Page(tid, question_cnt, number){
 
     //값 저장 후 페이지 이동
     localStorage.setItem("allEntries", JSON.stringify(existingEntries));
-    location.href = "http://127.0.0.1:5500/html"+'/'+number+'~'+".html?"+number;
+    location.href = "http://127.0.0.1:5500/htmlProject/fixHTML/html/"+number+'~'+".html?"+number;
 }
-// 저장된 세션 삭제
-function clearSesstion(){
-    localStorage.clear();
-    
+
+// 이전 페이지 -> 메인 페이지
+function main_page(){
+    location.href = "http://127.0.0.1:5500/Project/fixHTML/html/main.html";
 }
+
 // 저장된 데이터 JSP에 전달
 $(function dataSend(){
     var gt = localStorage.getItem("allEntries");
@@ -139,92 +140,8 @@ $(function dataSend(){
     return gt;
  });
 
- // --------------------------------------------------------------------------------------------------------------------------//
-
- /*다음 페이지로 이동과 동시에 Session에 값 추가*/
-function end_Page(tid, count, number) {
-  
-    question_cnt = (number - count) % 2
-  
-    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
-    if(existingEntries == null) existingEntries = [];
-
-    //Radio 버튼 값 불러오기
-    var survayValue1 = document.getElementsByName("survay1");
-    var survayValue2 = document.getElementsByName("survay2");
-    var survayValue3 = document.getElementsByName("survay3");
-    if (question_cnt == 0 ){
-        var survayValue4 = document.getElementsByName("survay3");
-    }
-    // 두 개의 값이 체크되어 있는지 검사
-    var chk_cnt = 0;
-
-    for(var i=0; i<survayValue1.length; i++){
-        if(survayValue1[i].checked==true){
-            chk_cnt++;
-        }
-        if(survayValue2[i].checked==true){
-            chk_cnt++;
-        }
-        if(survayValue3[i].checked==true){
-            chk_cnt++;
-        }
-        if (question_cnt == 0 ){
-            if(survayValue4[i].checked==true){
-                chk_cnt++;
-            }
-        }
-    }
-
-    // 두 가지 항목이 체크되어 있지 않으면 알람 후 재입력 유도
-    if ( question_cnt != 0 ){
-        if(chk_cnt<3){
-            alert('체크하세요');
-            return;
-        }
-    }
-    else{
-        if(chk_cnt<4){
-            alert('체크하세요');
-            return;
-        }
-    }
-
+// 저장된 세션 삭제
+function clearSesstion(){
+    localStorage.clear();
     
-    //라디오 버튼이 체크되었나 확인하기 위한 변수
-    for(var i = 0; i<survayValue1.length; i++){
-        //만약 라디오 버튼이 체크가 되어있다면 true
-        if(survayValue1[i].checked==true){
-            //버튼 값 알림 후 value push
-            alert(survayValue1[i].value);
-            existingEntries.push(survayValue1[i].value);
-        }
-       
-    }
-    for(var i = 0; i<survayValue2.length; i++){
-        if(survayValue2[i].checked==true){
-            //버튼 값 알림 후 value push
-            alert(survayValue2[i].value);
-            existingEntries.push(survayValue2[i].value);
-        }
-    }
-
-    for(var i = 0; i<survayValue3.length; i++){
-        if(survayValue3[i].checked==true){
-            alert(survayValue3[i].value);
-            existingEntries.push(survayValue3[i].value);
-        }
-    }
-    if ( question_cnt == 0){
-        for(var i = 0; i<survayValue4.length; i++){
-            if(survayValue4[i].checked==true){
-                alert(survayValue4[i].value);
-                existingEntries.push(survayValue4[i].value);
-            }
-        }
-    }
-
-    //값 저장 후 페이지 이동
-    localStorage.setItem("allEntries", JSON.stringify(existingEntries));
-    location.href = "http://127.0.0.1:5500/html/"+'end'+".html?"+number;
-};
+}
