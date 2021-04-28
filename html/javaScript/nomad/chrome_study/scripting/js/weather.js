56dc3073244058d0d94ce70eef10703a7abd4cdf
ParkDoHun <55828180+ParkDoHun@users.noreply.github.com>
2021-04-28 23:29:18 +0900

    Add files via upload

--- html/javaScript/nomad/chrome_study/scripting/js/weather.js
+++ html/javaScript/nomad/chrome_study/scripting/js/weather.js
@@ -1,13 +1,30 @@
 // https://home.openweathermap.org/api_keys
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "406ccf356586ca4dee1d05e50923c2af";
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        ).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature}℃ @ ${place}`
+        });
+        // then은 함수를 호출
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position) {
+    // 위도
     const latitude = position.coords.latitude;
+    // 경도
     const longitude = position.coords.longitude;
     const coordsObj = {
         // 객체.. 키를 적지 않으면 값과 키는 동일한 이름 (latitude = latitude: latitude)
@@ -15,6 +32,7 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -30,7 +48,9 @@ function loadCoords(){
     if (loadedCoords === null) {
         askForCoords();
     } else {
-        // getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+        // console.log(parseCoords);
     }
 }
 
