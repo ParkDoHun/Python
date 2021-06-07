dbc164d5ae47268d8463a03a2c720bff6a66f4df
ParkDoHun <55828180+ParkDoHun@users.noreply.github.com>
2021-06-07 23:43:24 +0900

    Add files via upload

---
+++ html/javaScript/Deep_Dive/07.Closure/Closure09.js
@@ -0,0 +1,14 @@
+// 자주 발생하는 실수
+var arr = [];
+
+for (var i = 0; i < 5; i++) {
+  arr[i] = function () {
+    return i;
+  };
+}
+
+for (var j = 0; j < arr.length; j++) {
+  console.log(arr[j]()); // 55555
+}
+
+// 배열 arr에 5개의 함수가 할당되고 각각의 함수는 순차적으로 0, 1, 2, 3, 4를 반환할 것으로 기대하겠지만 결과는 그렇지않다.
