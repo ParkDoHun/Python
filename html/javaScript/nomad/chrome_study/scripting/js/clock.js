84dd1436ebc3d4d6dd570f957e5912469107264f
ParkDoHun <55828180+ParkDoHun@users.noreply.github.com>
2021-06-27 13:47:42 +0900

    Update clock.js

--- html/javaScript/nomad/chrome_study/scripting/js/clock.js
+++ html/javaScript/nomad/chrome_study/scripting/js/clock.js
@@ -2,7 +2,7 @@ const clockContainer = document.querySelector('.js-clock');
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime () {
-    const date = new Date();        // new 있어야 작동
+    const date = new Date();        // Data 생성자 함수
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
@@ -13,7 +13,7 @@ function getTime () {
 
 function init () {
     getTime();
-    setInterval(getTime, 1000);     // 콜백 함수, 매 초 마다 실행
+    setInterval(getTime, 1000);     // 매 초 마다 실행
 }
 
 init();
